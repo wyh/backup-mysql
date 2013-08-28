9709970f595165a43c635780b22e2e673e06ca6c
Samuel <wyh@shanbay.com>
2013-08-28 15:13:51 +0800

    added the script and enrich readme

--- README.md
+++ README.md
@@ -2,3 +2,40 @@ backup-mysql
 ============
 
 a script of  utilizing percona innobackupex to automatically backup mysql data online
+
+
+### requirements 
+
+This script needs percona xtracbackup tool, which backups mysql online. For more information, please visit: [ percona xtrabackup ] (http://www.percona.com/software/percona-xtrabackup)  
+
+Before executing this script, a mysql user should be created. Example code below:
+
+```
+CREATE USER 'bkpuser'@'localhost' IDENTIFIED BY 's3cret';
+GRANT RELOAD, LOCK TABLES, REPLICATION CLIENT ON *.* TO 'bkpuser'@'localhost';
+FLUSH PRIVILEGES;
+
+```
+
+The secret will be used in the script as the parameter of password. You need to change this in the script.
+
+###  what does the script do?
+
+1. Make a full backup to mysql data on each 01, 08, 15, 23 of each month
+2. Make a daily incremental back to mysql data on the other days
+3. Automatically  remove the first backup if the second backup finished
+4. Compress and encrypt the backed up data to a remote server
+
+### how to run?
+
+1. Beforing running, make sure a back up directory available.
+2. Make sure root user can access to the remote server directly, that's, without password. You can not feed a password in crontab. Use ssh-copy-id on ubuntu servers.
+3. User on the remote server has the permission to write in the backup directory. Remote login as a root user is not recommended.
+4. change secret in the script before running
+4. run it. (1 forced to make full backup)
+    ```
+        sudo backup_mysql.sh /backup/dir remote@server /remote/backup/dir 1
+    ```
+### how to restore the data?
+
+Please read percona xtrabackup manual.
---
+++ backup_mysql.sh
@@ -0,0 +1,56 @@
+backup_dir=$1;
+remote_server=$2
+remote_backup_dir=$3
+forced=$4
+fullback_days="01 08 15 23"
+password="secret"
+
+cd $backup_dir;
+mkdir -p full;
+mkdir -p incr;
+ssh $remote_server " mkdir -p $remote_backup_dir/full; mkdir -p $remote_backup_dir/incr"
+
+fullbase_dir=`echo $backup_dir/full`;
+
+day=`date +'%F'|grep -oe '-[0-9][0-9]$'|sed 's/-//'`;
+if [ $forced -eq 1 ]; then 
+    # forced a full backup
+    day=`echo 01`
+fi
+match=`echo $fullback_days | grep -o $day`;
+
+if [ ! -z $match ]; then
+    # do a full back
+    echo "date of today is $day, belongs to $fullback_days. Make an full backup"
+    innobackupex --user=bkpuser --password=$password $fullbase_dir
+    if [ $? -eq 0 ]; then 
+        echo "successfully completed backup"
+        echo "zip and encrypt backup directory to remote server"
+        cd full;
+        fullbackup_dir=`ls -1 |tail -1`;
+        tar -cvzf - $fullbackup_dir | openssl des3 -salt -k $password | ssh $remote_server "cat > $remote_backup_dir/full/$fullbackup_dir.tgz.des3"
+        backups=`ls -1|wc -l`
+        if [ $backups -gt 1 ]; then
+            echo "More than  one backup found."
+            echo "Going to delete one to save the space"
+            rm -rf `ls -1|head -1`
+        fi
+        cd $backup_dir;
+    fi
+
+else
+    # do an incremental backup
+    echo "date of today is $day, not in $fullback_days. Make an incremental backup"
+    fullbackup_dir=`ls -1 $backup_dir/full/|tail -1`;
+    echo "fullbackup_dir: $fullbackup_dir"
+    innobackupex --user=bkpuser --password=test --incremental $backup_dir/incr/ --incremental-basedir=$fullbase_dir/$fullbackup_dir
+    if [ $? -eq 0 ]; then
+        echo "successfully made an incremental backup"
+        echo "zip and encrypt this newly created incremental backup to remote server"
+        cd incr;
+        incr_dir=`ls -1 |tail -1`;
+        tar -cvzf - $incr_dir | openssl des3 -salt -k $password | ssh $remote_server "cat > $remote_backup_dir/incr/$incr_dir.tgz.des3"
+        echo "completed moving incremental backup to remote server: $remote_server"
+    fi;
+    cd $backup;
+fi
